6677a0ca15bdae959343aa8a974ee4af6dbe20f9
kindsenior <kunio.einstein@gmail.com>
2015-07-23 13:45:52 +0900

    modify hrpsys sequence file export function to check iklinks with keyframes

--- plugins/HrpsysSequenceFileExportPlugin/HrpsysSequenceFileExportPlugin.cpp
+++ plugins/HrpsysSequenceFileExportPlugin/HrpsysSequenceFileExportPlugin.cpp
@@ -14,49 +14,38 @@ void HrpsysSequenceFileExportPlugin::HrpsysSequenceFileExport(){
   ItemList<BodyItem> bodyItems = ItemTreeView::mainInstance()->checkedItems<BodyItem>();
   BodyPtr body = bodyItems[0]->body();// ロボットモデルは１個のみチェック
 
-  // 足先リンク名取得
-  Link *lFootLink, *rFootLink;
-  UtilPlugin::getFootLink( &lFootLink, &rFootLink, body );
+  LeggedBodyHelperPtr lgh = getLeggedBodyHelper(body);
 
-  ItemList<PoseSeqItem> poseSeqItems = ItemTreeView::mainInstance()->selectedItems<Item>();
+  PoseSeqItem* poseSeqItem = ItemTreeView::mainInstance()->selectedItems<PoseSeqItem>()[0];
+  PoseSeqPtr poseSeq = poseSeqItem->poseSeq();
 
-  BodyMotionItem* bodyMotionItem = poseSeqItems[0]->bodyMotionItem();
+  BodyMotionItem* bodyMotionItem = poseSeqItem->bodyMotionItem();
   BodyMotionPtr motion = bodyMotionItem->motion();
 
-  string poseSeqPathString = poseSeqItems[0]->filePath();
+  string poseSeqPathString = poseSeqItem->filePath();
   boost::filesystem::path poseSeqPath(poseSeqPathString);
   cout << " parent_path:" << poseSeqPath.parent_path().string() << " basename:" << getBasename(poseSeqPath) << endl;
 
   stringstream ss;
-  ss << poseSeqPath.parent_path().string() << "/" << poseSeqItems[0]->name() << ".optionaldata";
+  ss << poseSeqPath.parent_path().string() << "/" << poseSeqItem->name() << ".optionaldata";
   FILE* fp = fopen(ss.str().c_str(),"w"); if( fp == NULL ){ cout << "\x1b[31m" << "optionaldata file("<< ss.str() << ") open error" << "\x1b[m" << endl; return; }
 
   double frameRate = motion->frameRate();
 
-  for(PoseSeq::iterator poseItr = poseSeqItems[0]->poseSeq()->begin(); poseItr != (--poseSeqItems[0]->poseSeq()->end()); ++poseItr){
-      PoseSeq::iterator nextItr = poseItr; ++nextItr;
-      // cout << poseSeqItems[0]->poseSeq()->end()->time() << endl;// iteratorのendのtimeは0
+  for( PoseSeq::iterator prevPoseIter = poseSeq->begin(), poseIter = poseSeq->begin(); poseIter != poseSeq->end(); prevPoseIter = poseIter ){
+      incContactPose( poseIter, poseSeq, body );
+      cout << "phase:" << prevPoseIter->time() << "[sec]->" << poseIter->time() << "[sec]" << endl;
 
-      // PosePtr取得
-      PosePtr curPose = poseItr->get<Pose>();
-      PosePtr nextPose = nextItr->get<Pose>();
-
-      // for( int j = 0; j < curPose->numJoints(); ++j ){ body->joint(j)->q() =  curPose->jointPosition(j); }
-      // body->calcForwardKinematics();
-
-      // for( std::map<int, Pose::LinkInfo>::iterator linkItr = curPose->ikLinkBegin(); linkItr != curPose->ikLinkEnd(); ++linkItr ){
-      //     cout << linkItr->first << ":" << linkItr->second.p << " ";
-      // }cout << endl << endl;      
-
-      int lFootState = 1, rFootState = 1;
-      curPose->ikLinkInfo(lFootLink->index())->p;
-      if( (curPose->ikLinkInfo(lFootLink->index())->p - nextPose->ikLinkInfo(lFootLink->index())->p).norm() > 1e-6 ) lFootState = 0;
-      if( (curPose->ikLinkInfo(rFootLink->index())->p - nextPose->ikLinkInfo(rFootLink->index())->p).norm() > 1e-6 ) rFootState = 0;
+      std::vector<int> contactStates;
+      for( int i = 0; i < lgh->numFeet(); ++i ){
+          contactStates.push_back( getPrevContactState( poseIter, poseSeq, lgh->footLink(i)->index() ) );
+      }
 
       // 同じ時刻のキーポーズが連続するとダメ
-      for( int i = (int) poseItr->time()*frameRate; i < nextItr->time()*frameRate; ++i ){
-          fprintf(fp,"%lf %d %d 0 0   5 5 5 5\n", (double)i/frameRate, rFootState, lFootState);
+      for( int i = (int) (prevPoseIter->time()*frameRate); i < poseIter->time()*frameRate; ++i ){
+          fprintf( fp,"%lf %d %d 0 0   5 5 5 5\n", (double)i/frameRate, contactStates[0], contactStates[1] );
       }
+      cout << endl << endl;
   }
 
   fclose(fp);
