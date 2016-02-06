7be2720cb692c090c7c04008bdfc38cacfae0293
kindsenior <kunio.einstein@gmail.com>
2016-02-06 12:53:39 +0900

    add multi contact stabilizer plugin

---
+++ plugins/MultiContactStabilizerPlugin/MultiContactStabilizerPlugin.cpp
@@ -0,0 +1,93 @@
+/**
+   @author Kunio Kojima
+*/
+#include "MultiContactStabilizerPlugin.h"
+
+using namespace boost;
+using namespace cnoid;
+using namespace std;
+
+void MultiContactStabilizerPlugin::MultiContactStabilizer(){
+    stringstream ss,fnamess;
+    MessageView::instance()->putln("MultiContactStabilizer called !");
+    cout << "MultiContactStabilizer()" << endl;
+
+    ItemList<PoseSeqItem> selectedItemList = ItemTreeView::mainInstance()->selectedItems<Item>();
+    if(selectedItemList.empty()){cout << "Select PoseSeqItem!!" << endl; return;}
+    BodyItem* pBodyItem = selectedItemList[0]->findOwnerItem<BodyItem>();
+    mBody = pBodyItem->body();
+    cout << "Robot:" << mBody->name() << endl;
+    PoseSeqItem* pPoseSeqItem = selectedItemList[0];
+    cout << pPoseSeqItem->name() << endl;
+    mPoseSeqPath = boost::filesystem::path(pPoseSeqItem->filePath());
+
+    PoseSeqPtr poseSeq = pPoseSeqItem->poseSeq();
+    BodyMotionItem* pBodyMotionItem = pPoseSeqItem->bodyMotionItem();
+    BodyMotionPtr motion = pBodyMotionItem->motion();
+
+    // BodyMotion作成
+    BodyMotionGenerationBar* bmgb = BodyMotionGenerationBar::instance();
+    PoseProvider* provider = pPoseSeqItem->interpolator().get();
+    bmgb->shapeBodyMotion(mBody, provider, pBodyMotionItem, true);
+    cout << " Generated motion" << endl;
+
+    frameRate = motion->frameRate();
+    dt = 1.0/frameRate;
+    numFrames = motion->numFrames();
+
+
+
+    // 左右足Pathを設定
+    // mJpl = getCustomJointPath( mBody, mBody->rootLink(), mLFootLink );
+    // mJpr = getCustomJointPath( mBody, mBody->rootLink(), mRFootLink );
+
+    //     BodyMotionItem* bodyMotionItem = pPoseSeqItem->bodyMotionItem();
+    //     BodyMotionPtr motion = bodyMotionItem->motion();
+
+    //     // BodyMotion作成
+    //     BodyMotionGenerationBar* bmgb = BodyMotionGenerationBar::instance();// インスタンス生成
+    //     PoseProvider* provider = pPoseSeqItem->interpolator().get();
+    //     bmgb->shapeBodyMotion(mBody, provider, bodyMotionItem, true);
+    //     cout << " Generated motion" << endl;
+
+    //     // 目標運動量軌道作成
+    //     Vector3Seq refPSeq,refLSeq;
+    //     Vector3d initDCM,endDCM,initL,endL;// initDCMとendPを明示的に0で初期化していない
+    //     generateRefPLSeq( bodyMotionItem, pPoseSeqItem->poseSeq(), initDCM, endDCM, initL, endL, refPSeq, refLSeq );
+    //     // loadRefPLSeq( bodyMotionItem, pPoseSeqItem->poseSeq(), initP, endP, initL, endL, refPSeq, refLSeq );
+    //     cout << " Generated ref P/L" << endl;
+
+    //         fnamess.str("");
+    //         fnamess << mPoseSeqPath.parent_path().string() << "/" << getBasename(mPoseSeqPath) << "_RMC_PL_" << motion->frameRate() << "fps_" << i << ".dat";
+    //         ofstream ofs( fnamess.str().c_str() );
+    //         if( !ofs ){ cerr << "File Open Error" << endl; return;}
+    //         ofs << "time CMx CMy CMz Px Py Pz Lx Ly Lz" << endl;
+
+    //         double dt = 1.0/motion->frameRate();
+
+    //         // motion loop 計算 step 2 〜 6
+    //         // for(int currentFrame = 0; currentFrame < 1; ++currentFrame){
+    //         for(int currentFrame = 0; currentFrame < motion->numFrames(); ++currentFrame){
+    //             ofs << currentFrame * dt;
+
+    //             // prevFrame,nextFrame設定
+    //             int prevFrame = std::max(currentFrame - 1, 0);
+    //             int nextFrame = std::min(currentFrame + 1, motion->numFrames() - 1);
+
+    //             // motion->frame(nextFrame) >> *mBody;
+    //             // mBody->calcForwardKinematics();
+
+    //             motion->frame(nextFrame) << *mBody;// frame更新
+            
+    //         }// end motion loop
+
+    //         ofs.close();
+
+    //     }
+    // }// end poseSeqItem loop
+
+    cout << "Finished MultiContactStabilizer" << endl;
+}
+
+CNOID_IMPLEMENT_PLUGIN_ENTRY(MultiContactStabilizerPlugin)
+
---
+++ plugins/MultiContactStabilizerPlugin/MultiContactStabilizerPlugin.h
@@ -0,0 +1,81 @@
+/**
+   @author Kunio Kojima
+*/
+
+#include <iostream>
+#include <sstream>
+#include <fstream>
+#include <map>
+
+#include <unistd.h>
+
+#include <cnoid/Plugin>
+#include <cnoid/ItemTreeView>
+#include <cnoid/BodyItem>
+#include <cnoid/ToolBar>
+#include <boost/bind.hpp>
+#include <cnoid/MessageView>
+#include <cnoid/YAMLReader>
+
+#include <cnoid/src/Body/Jacobian.h>
+#include <cnoid/src/PoseSeqPlugin/PoseSeqItem.h>
+#include <cnoid/src/PoseSeqPlugin/gettext.h>
+#include <cnoid/src/PoseSeqPlugin/BodyMotionGenerationBar.h>
+#include <cnoid/Vector3SeqItem>
+
+#include <cnoid/PoseProvider>
+#include <cnoid/BodyMotionPoseProvider>
+#include <cnoid/PoseProviderToBodyMotionConverter>
+#include <cnoid/BodyMotionUtil>
+#include <cnoid/TimeBar>
+#include <cnoid/Archive>
+#include <cnoid/MenuManager>
+#include <cnoid/MainWindow>
+#include <cnoid/SpinBox>
+#include <cnoid/Button>
+#include <QDialog>
+#include <QDialogButtonBox>
+#include <boost/format.hpp>
+#include <set>
+
+// #include <DynamicsPlugin/DynamicsPlugin.h>
+#include <cnoid/FileUtil>
+
+#include <UtilPlugin/UtilPlugin.h>
+
+namespace cnoid{
+
+    class MultiContactStabilizerPlugin : public Plugin
+    {
+    public:
+        BodyPtr mBody;
+        boost::filesystem::path mPoseSeqPath;
+        int frameRate;
+        double dt;
+        int numFrames;
+
+        MultiContactStabilizerPlugin() : Plugin("MultiContactStabilizer")
+        {
+            require("Body");
+            require("PoseSeq");
+            // require("Dynamics");
+        }
+    
+        virtual bool initialize()
+        {
+            ToolBar* bar = new ToolBar("MultiContactStabilizer");
+
+            bar->addButton("MCS")
+                ->sigClicked().connect(boost::bind(&MultiContactStabilizerPlugin::MultiContactStabilizer, this));
+
+            bar->setVisibleByDefault(true);// 効かない?
+            addToolBar(bar);
+
+            return true;
+        }
+
+        void MultiContactStabilizer();
+
+    };
+
+}
