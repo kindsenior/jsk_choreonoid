082d63a808669d8d46bc1a476a375bf7e2766ab0
kindsenior <kunio.einstein@gmail.com>
2016-02-07 12:12:53 +0900

    implement function for generating initial P,L seq

--- plugins/MultiContactStabilizerPlugin/MultiContactStabilizerPlugin.cpp
+++ plugins/MultiContactStabilizerPlugin/MultiContactStabilizerPlugin.cpp
@@ -7,6 +7,37 @@ using namespace boost;
 using namespace cnoid;
 using namespace std;
 
+void MultiContactStabilizerPlugin::generateSeq(){
+    stringstream ss;
+    ss << mPoseSeqPath.stem().string() << "_MCS_initPL_" << frameRate << "fps.dat";
+    ofstream ofs( ((filesystem::path) mPoseSeqPath.parent_path() / ss.str()).string().c_str() );
+    ofs << "time initCM initCMy initCMz initPx initPy initPz initLx initLy initLz" << endl;
+
+    Vector3SeqPtr initCMSeqPtr = mBodyMotionItem->motion()->getOrCreateExtraSeq<Vector3Seq>("initCM");
+    Vector3SeqPtr initPSeqPtr = mBodyMotionItem->motion()->getOrCreateExtraSeq<Vector3Seq>("initP");
+    Vector3SeqPtr initLSeqPtr = mBodyMotionItem->motion()->getOrCreateExtraSeq<Vector3Seq>("initL");
+    for(int i = 0; i < numFrames; ++i){
+        updateBodyState(body, motion, i);
+        body->calcForwardKinematics(true,true);
+
+        Vector3d P,L,CM;
+        CM = body->calcCenterOfMass();
+        body->calcTotalMomentum(P,L);
+        L -= CM.cross(P);// convert to around CoM
+
+        initCMSeqPtr->at(i) =  CM;
+        initPSeqPtr->at(i) = P;
+        initLSeqPtr->at(i) = L;
+
+        ofs << i*dt << " " << CM.transpose() <<  " " << P.transpose() << " " << L.transpose() << " " << endl;
+    }
+    setSubItem("initCM", initCMSeqPtr, mBodyMotionItem);
+    setSubItem("initP", initPSeqPtr, mBodyMotionItem);
+    setSubItem("initL", initLSeqPtr, mBodyMotionItem);
+
+    ofs.close();
+}
+
 void MultiContactStabilizerPlugin::MultiContactStabilizer(){
     stringstream ss,fnamess;
     MessageView::instance()->putln("MultiContactStabilizer called !");
@@ -36,6 +67,7 @@ void MultiContactStabilizerPlugin::MultiContactStabilizer(){
     dt = 1.0/frameRate;
     numFrames = motion->numFrames();
 
+    generateSeq();
 
 
     // 左右足Pathを設定
--- plugins/MultiContactStabilizerPlugin/MultiContactStabilizerPlugin.h
+++ plugins/MultiContactStabilizerPlugin/MultiContactStabilizerPlugin.h
@@ -77,6 +77,7 @@ namespace cnoid{
             return true;
         }
 
+        void generateSeq();
         void MultiContactStabilizer();
 
     };
