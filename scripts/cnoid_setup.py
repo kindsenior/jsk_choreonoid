7b976ddd1f9f6894b242f5710f1536961320bc06
kindsenior <kunio.einstein@gmail.com>
2016-02-28 14:21:52 +0900

    make python common instance in python setting script

--- scripts/cnoid_setup.py
+++ scripts/cnoid_setup.py
@@ -5,3 +5,9 @@ from cnoid.Util import *
 
 import os
 import roslib
+import inspect
+from pprint import pprint
+
+robot = ItemTreeView.instance().rootItem().childItem().childItem().body()
+robotItem = ItemTreeView.instance().rootItem().childItem().childItem()
+poseSeqItem = robotItem.childItem()
