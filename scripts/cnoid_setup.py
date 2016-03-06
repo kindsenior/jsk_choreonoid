7eb3f27aee40fecb3eae620a49156e90280e43dd
kindsenior <kunio.einstein@gmail.com>
2016-03-06 22:29:40 +0900

    cnoid_setup: import cnoid.Body and numpy

--- scripts/cnoid_setup.py
+++ scripts/cnoid_setup.py
@@ -1,5 +1,6 @@
 from cnoid.Base import *
 from cnoid.BodyPlugin import *
+from cnoid.Body import *
 from cnoid.PoseSeqPlugin import *
 from cnoid.Util import *
 
@@ -7,6 +8,7 @@ import os
 import roslib
 import inspect
 from pprint import pprint
+from numpy import *
 
 # robot = ItemTreeView.instance().rootItem().childItem().childItem().body()
 # robotItem = ItemTreeView.instance().rootItem().childItem().childItem()
