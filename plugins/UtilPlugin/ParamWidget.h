be6b17ac6bddffce5fc519643e86ad0c67a9caf7
kindsenior <kunio.einstein@gmail.com>
2016-07-17 19:45:45 +0900

    UtilPlugin: add SpinVectorParamWidget class

--- plugins/UtilPlugin/ParamWidget.h
+++ plugins/UtilPlugin/ParamWidget.h
@@ -4,6 +4,10 @@
 #pragma once
 
 #include <sstream>
+#include <vector>
+#include <iostream>
+
+#include <boost/algorithm/string.hpp>
 
 #include <cnoid/ToolBar>
 #include <cnoid/Dialog>
@@ -104,6 +108,105 @@ public:
     {
         int idx;
         if((idx = findText(QString(param.c_str()))) != -1) setCurrentIndex(idx);
+    }
+};
+
+class SpinVectorParamWidget: public ParamWidget,
+                             public std::vector<SpinBox*>
+{
+protected:
+    // std::vector<SpinBox*> spinBoxVector;//ポインタじゃないとerror: use of deleted function
+    int maxRange;
+    QBoxLayout* parentLayout;
+
+    void addAllSpinWidget()
+    {
+        if(parentLayout){
+            // for(std::vector<SpinBox*>::iterator iter = spinBoxVector.begin(); iter != spinBoxVector.end(); ++iter){
+            //     layout->addWidget((SpinBox*) *iter);
+            // }
+            for(std::vector<SpinBox*>::iterator iter = this->begin(); iter != this->end(); ++iter){
+                parentLayout->addWidget((SpinBox*) *iter);
+            }
+        }
+    }
+
+    void removeAllSpinWidet()
+    {
+        for(std::vector<SpinBox*>::iterator iter = this->begin(); iter != this->end(); ++iter){
+            // parentLayout->removeWidget((SpinBox*) *iter);// removeWidget()できない Qtのバグ?
+            delete *iter;
+        }
+    }
+
+public:
+    SpinVectorParamWidget()
+        :ParamWidget(),
+         std::vector<SpinBox*>()
+    {
+        maxRange = 1000;
+        parentLayout = NULL;
+    }
+
+    void addToLayout(QBoxLayout* layout)
+    {
+        parentLayout = layout;
+        ParamWidget::addToLayout(layout);
+        addAllSpinWidget();
+    }
+
+    std::string getParam()
+    {
+        std::stringstream ss;
+        // for(std::vector<SpinBox*>::iterator iter = spinBoxVector.begin(); iter != spinBoxVector.end(); ++iter){
+        std::vector<SpinBox*>::iterator iter = this->begin();
+        while(true){
+            ss << (*iter)->value();
+            if(++iter == this->end()) break;
+            else ss << ",";
+        }
+        return ss.str();
+    }
+    void setParam(std::string param)
+    {
+        // spinBoxVector.clear();
+        removeAllSpinWidet();
+        // this->clear();
+        std::vector<SpinBox*>::clear();
+        std::vector<std::string> stringVec;
+        boost::algorithm::split(stringVec, param, boost::is_any_of(","));
+        for(std::vector<std::string>::iterator iter = stringVec.begin(); iter != stringVec.end(); ++iter){
+            SpinBox* sb = new SpinBox();
+            sb->setRange(0,maxRange);
+            sb->setValue(std::atoi(iter->c_str()));
+            // spinBoxVector.push_back(sb);
+            this->push_back(sb);
+        }
+        // updateAllWidget();
+        addAllSpinWidget();
+    }
+
+    void setValue(std::vector<int> vec)
+    {
+        // spinBoxVector.clear();
+        // this->clear();
+        std::vector<SpinBox*>::clear();
+        for(std::vector<int>::iterator iter = vec.begin(); iter != vec.end(); ++iter){
+            SpinBox* sb = new SpinBox();
+            sb->setRange(0,maxRange);
+            sb->setValue(*iter);
+            // spinBoxVector.push_back(sb);
+            this->push_back(sb);
+        }
+        addAllSpinWidget();
+    }
+    std::vector<int> value()
+    {
+        std::vector<int> ret;
+        for(std::vector<SpinBox*>::iterator iter = this->begin(); iter != this->end(); ++iter){
+            ret.push_back((*iter)->value());
+        }
+        return ret;
     }
 };
 
