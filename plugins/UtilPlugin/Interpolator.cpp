834606f4e5f3cb317c1569b8107852879cdd4de1
kindsenior <kunio.einstein@gmail.com>
2018-01-02 21:13:43 +0900

    [UP] add Interpolator and CubicSplineInterpolator class

--- plugins/UtilPlugin/Interpolator.cpp
+++ plugins/UtilPlugin/Interpolator.cpp
@@ -39,3 +39,31 @@ void cnoid::setCubicSplineInterpolation(std::vector<Vector3d>& a, const Vector3d
 //     a4 = -ddx1/T2    + 3*ddx0/(2*T2) + 7*dx1/T3 + 8*dx0/T3 - 15*x1/T4 + 15*x0/T4;
 //     a5 = ddx1/(2*T3) - ddx0/(2*T3)   - 3*dx1/T4 - 3*dx0/T4 + 6*x1/T5  - 6*x0/T5;
 // }
+
+void CubicSplineInterpolator::calcCoefficients(const VectorXd& x0, const VectorXd& dx0, const VectorXd& x1, const VectorXd& dx1, const double duration)
+{
+    numDimensions = x0.size();
+    while(coefficientVec.size() < numDegree) coefficientVec.push_back(VectorXd::Zero(numDimensions));
+
+    coefficientVec[0] = x0;
+    coefficientVec[1] = dx0;
+    MatrixXd A(2,numDimensions);
+    Matrix2d T;
+    T <<
+        pow(duration, 2), pow(duration, 3),
+        2 * duration, 3 * pow(duration, 2);
+    MatrixXd P(2,numDimensions);
+    P.row(0) = x1.transpose() - x0.transpose()  - dx0.transpose() * duration;
+    P.row(1) = dx1.transpose() - dx0.transpose();
+    A = T.inverse() * P;
+    coefficientVec[2] = A.block(0,0, 1,numDimensions).transpose();
+    coefficientVec[3] = A.block(1,0, 1,numDimensions).transpose();
+}
+
+VectorXd CubicSplineInterpolator::x(double t){
+    return coefficientVec[0] + coefficientVec[1] * t + coefficientVec[2] * pow(t,2) + coefficientVec[3] * pow(t,3);
+}
+
+VectorXd CubicSplineInterpolator::dx(double t){
+    return coefficientVec[1] + 2 * coefficientVec[2] * t + 3 * coefficientVec[3] * pow(t,2);
+}
--- plugins/UtilPlugin/Interpolator.h
+++ plugins/UtilPlugin/Interpolator.h
@@ -10,4 +10,30 @@ namespace cnoid {
 
 void setCubicSplineInterpolation(std::vector<Vector3d>& a, const Vector3d& x0, const Vector3d& dx0, const Vector3d& x1, const Vector3d& dx1, const double tau);
 
+class Interpolator
+{
+protected:
+    int numDimensions;
+
+public:
+    std::vector<VectorXd> coefficientVec;
+
+    Interpolator(){}
+};
+
+class CubicSplineInterpolator : public Interpolator
+{
+public:
+    int numDegree;
+
+    CubicSplineInterpolator() : Interpolator(){
+        numDegree = 4;
+    }
+
+    void calcCoefficients(const VectorXd& x0, const VectorXd& dx0, const VectorXd& x1, const VectorXd& dx1, const double duration);
+
+    VectorXd x(double t);
+    VectorXd dx(double t);
+};
+
 }
