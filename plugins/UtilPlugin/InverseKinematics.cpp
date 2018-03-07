5267e1b76a08e41dd287132cebb5ebbee92c5c81
kindsenior <kunio.einstein@gmail.com>
2018-03-08 02:26:58 +0900

    [UP] add function for calculating SRInverse and inverseJacobian

--- plugins/UtilPlugin/InverseKinematics.cpp
+++ plugins/UtilPlugin/InverseKinematics.cpp
@@ -26,5 +26,33 @@ t_matrix cnoid::PseudoInverse(const t_matrix& m, const double &tolerance=1.e-6)
     return svd.matrixV()*sigma_inv.asDiagonal()*svd.matrixU().transpose();
 }
 
-
 template Eigen::MatrixXd cnoid::PseudoInverse(const Eigen::MatrixXd&, const double&);
+
+// cannot call default argument by reference so have to overload function
+Eigen::MatrixXd cnoid::SRInverse(const Eigen::MatrixXd& m, const double weight, const double manipulability_const)
+{
+    Eigen::MatrixXd w;
+    return SRInverse(m, w, weight, manipulability_const);
+}
+
+Eigen::MatrixXd cnoid::SRInverse(const Eigen::MatrixXd& m, Eigen::MatrixXd& weight_mat, const double weight, const double manipulability_const)
+{
+    const int numCols = m.cols();
+    if ( weight_mat.cols() != numCols || weight_mat.rows() != numCols ) {
+        weight_mat = Eigen::MatrixXd::Identity(numCols, numCols);
+    }
+
+    double manipulability = sqrt((m*m.transpose()).determinant());
+    return weight_mat*m.transpose() * ((m*weight_mat*m.transpose() + weight*exp(-manipulability/manipulability_const) * Eigen::MatrixXd::Identity(m.rows(),m.rows())).inverse());
+}
+
+// Eigen::MatrixXd cnoid::calcInverseJacobian(const Eigen::MatrixXd& m)
+Eigen::MatrixXd cnoid::inverseJacobian(JointPathPtr& jp)
+{
+    MatrixXd J;
+    jp->calcJacobian(J);
+    const int numJoints = jp->numJoints();
+    Eigen::MatrixXd weight_mat = Eigen::MatrixXd::Identity(numJoints, numJoints);
+
+    return SRInverse(J, weight_mat);
+}
--- plugins/UtilPlugin/InverseKinematics.h
+++ plugins/UtilPlugin/InverseKinematics.h
@@ -6,11 +6,16 @@
 #include <iostream>
 
 #include <cnoid/src/Util/EigenTypes.h>
+#include <cnoid/JointPath>
 
 namespace cnoid {
 
 template <typename t_matrix>
 t_matrix PseudoInverse(const t_matrix& m, const double &tolerance=1.e-6);
 
+Eigen::MatrixXd SRInverse(const Eigen::MatrixXd& m, const double weight = 1, const double manipulability_const = 0.01);
+Eigen::MatrixXd SRInverse(const Eigen::MatrixXd& m, Eigen::MatrixXd& weight_mat, const double weight = 1, const double manipulability_const = 0.01);
+
+Eigen::MatrixXd inverseJacobian(JointPathPtr& jp);
 
 }
