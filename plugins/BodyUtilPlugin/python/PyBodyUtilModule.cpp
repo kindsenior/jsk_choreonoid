5d04458962ea18aa3e28dcb265c5933c47f65520
kindsenior <kunio.einstein@gmail.com>
2019-07-09 19:23:31 +0900

    [BU] add DrawInterface class and python interface

--- plugins/BodyUtilPlugin/python/PyBodyUtilModule.cpp
+++ plugins/BodyUtilPlugin/python/PyBodyUtilModule.cpp
@@ -18,4 +18,17 @@ BOOST_PYTHON_MODULE(BodyUtil)
     def("links", links);
     def("jointList", jointList);
     def("angleVector", angleVector);
+
+    class_< DrawInterface, DrawInterfacePtr, bases<Referenced> >("DrawInterface", init<Vector3f>())
+        .def("setLineWidth", &DrawInterface::setLineWidth)
+        .def("setColor", &DrawInterface::setColor)
+        .def("show", &DrawInterface::show)
+        .def("hide", &DrawInterface::hide)
+        .def("drawLine", &DrawInterface::drawLine)
+        .def("drawArc", &DrawInterface::drawArc)
+        .def("drawArrow", &DrawInterface::drawArrow)
+        .def("drawArcArrow", &DrawInterface::drawArcArrow)
+        .def("drawLineArcArrow", &DrawInterface::drawLineArcArrow)
+        .def("drawArrowTip", &DrawInterface::drawArrowTip)
+        ;
 }
